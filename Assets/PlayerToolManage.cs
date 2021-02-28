f079ec1f1b9819d1c3b687aff7e90ec574a60ce7
griffindMIT <drakeg@mit.edu>
2021-02-27 22:18:52 -0500

    weapon system
    -added first original art
    -added tool UI
    -tool switching animation (janky + hardcoded)

---
+++ Assets/PlayerToolManage.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerToolManage : MonoBehaviour
+{
+    public static int activetool=1;
+    public static int nexttool=1;
+    public static bool swingtool=false;
+    public static bool changetool=false;
+    // Start is called before the first frame update
+    void Start(){}
+
+    // Update is called once per frame
+    void Update(){
+      // Take nexttool input
+      if (Input.GetKeyDown("1")){
+        nexttool=1;
+      }else if (Input.GetKeyDown("2")){
+        nexttool=2;
+      }
+      
+      //Check nextool
+      if (!(changetool)){
+        if (nexttool==activetool){
+          //they ARE the same tool...
+            if(!(swingtool)){
+              //check clix
+            }
+        }else{
+          //they ARE NOT the same tool...
+          if(!(swingtool)){
+            changetool = true;
+            //other script changes #
+          }
+        }
+      }
+    }
+}
---
+++ Assets/ToolAnimation.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToolAnimation : MonoBehaviour
+{
+
+    private int activetool=0;
+    private int nexttool=0;
+    private bool swingtool=false;
+    private bool changetool=false;
+    private Vector3 toolvector1;
+    private Vector3 origpos;
+    private Vector3 finpos;
+    private bool godown=true;
+    public float switchspeed=500f;
+    public Texture weapon2;
+    private RawImage toolskin;
+    // Start is called before the first frame update
+    void Start()
+    {
+      toolvector1 = new Vector3(0f,-1f,0f);
+      origpos = new Vector3(535f,86f,0f); //transform.position; //new Vector3(200f,-65f,0f);
+      finpos = new Vector3(200f,-120f,0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      //update from shared script
+      activetool = PlayerToolManage.activetool;
+      nexttool = PlayerToolManage.nexttool;
+      swingtool = PlayerToolManage.swingtool;
+      changetool = PlayerToolManage.changetool;
+
+      if (changetool){
+        //Do thing
+        if (godown){
+          transform.position += toolvector1*Time.deltaTime*switchspeed;
+        }else{
+          transform.position -= toolvector1*Time.deltaTime*switchspeed;
+        }
+        //Check thing
+        if (transform.position.y < finpos.y){
+          //at bottom
+          godown = false;
+          //change image
+          toolskin = gameObject.GetComponent<RawImage>();
+          toolskin.texture = weapon2;
+          //update origpos from database
+
+        }else if( (godown==false) && transform.position.y > origpos.y){
+          transform.position = origpos; //nuance changing to "new tool"
+          godown = true;
+          PlayerToolManage.changetool = false;
+          PlayerToolManage.activetool=nexttool;
+        }
+      }
+
+      //update other script
+    }
+}
